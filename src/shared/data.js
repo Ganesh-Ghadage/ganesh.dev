c98f8af9ed68f84b09d19ddb7d44276ed8ad2dd7
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-02-18 23:07:13 +0530

    Initialize portfolio website with Vite, React, and Tailwind CSS setup

---
+++ src/shared/data.js
@@ -0,0 +1,117 @@
+export const projects = [
+    {
+        title: "Car Marketplace",
+        date: "Sep 2024",
+        link: "https://car-marketplace-jade.vercel.app/",
+        desciption: {
+            title: "A demo web app where users can browse and list cars.",
+            point: [
+                "A web application built using React, Tailwind CSS, and Vite to provide a seamless user experience for browsing and listing cars.",
+                "Integrated Drizzle ORM for database management, Clerk for secure user authentication, and Firebase storage for storing listing images.",
+                "Utilized Axios for API requests and deployed on Vercel for fast, scalable performance.",
+                "This project showcases full-stack development skills with a focus on modern web technologies."
+            ]
+        },
+        technologies: ["React", "Tailwind CSS", "Vite", "Drizzle ORM", "Clerk", "Firebase", "Axios", "Vercel"]
+    },
+    {
+        title: "Blog Your Thoughts",
+        date: "Aug 2024",
+        link: "https://blog-your-thoughts-liart.vercel.app/",
+        desciption: {
+            title: "A sample blog web application includes features typical of a blogging platform.",
+            point: [
+                "BlogYourThoughts is a web application designed for users to create, manage, and share blog posts.",
+                "The app utilizes React to build a dynamic and interactive user interface, while Vite speeds up development with fast Hot Module Replacement (HMR). Tailwind CSS ensures responsive and visually appealing design.",
+                "React Router facilitates smooth navigation between different pages. Axios handles API requests for fetching and submitting content, and React Icons provides customizable icons to enhance the UI. Redux Toolkit manages user authentication.",
+                "Appwrite is used for user management, database operations, and file storage.",
+                "This blend of technologies delivers a robust and user-friendly blogging experience."
+            ]
+        },
+        technologies: ["React", "Tailwind CSS", "Vite", "React Router", "React Icons", "Redux Toolkit", "Axios", "Appwrite "]
+    },
+    {
+        title: "News Letter",
+        date: "Aug 2024",
+        link: "https://news-letter-lime.vercel.app/",
+        desciption: {
+            title: "A Sample web app features news and headlines",
+            point: [
+                "Developed a responsive and dynamic newsletter application using React, Tailwind CSS, and Vite.",
+                "This project integrates Axios for API requests, and Redux Toolkit for state management.",
+                "React Query (or custom hooks for data fetching) simplifies the process of fetching, caching, and synchronizing data from APIs."
+            ]
+        },
+        technologies: ["React", "Tailwind CSS", "Vite", "React Router", "React Icons", "Redux Toolkit", "Axios", "Appwrite "]
+    },
+    {
+        title: "Linked AI Chrome Extension",
+        date: "Nov 2024",
+        desciption: {
+            title: "Developed a Chrome extension leveraging Google Gemini AI to generate professional context-aware LinkedIn replies.",
+            point: [
+                "Developed a Chrome extension leveraging Google Gemini AI to generate professional, context-aware LinkedIn replies.",
+                "Reduced user response time by up to 50%, enhancing communication efficiency.",
+                "Built using React.js, TypeScript, WXT Framework, and Tailwind CSS for a robust and seamless experience.",
+                "Integrated directly into LinkedIn's messaging interface for smooth user interaction."
+            ]
+        },
+        technologies: ["React.js", "TypeScript", "WXT Framework", "Google Gemini API", "Tailwind CSS", "Vite", "Git", "GitHub"]
+    },
+]
+
+export const exprience = [
+    {
+        title: "System Engineer",
+        company: "Infosys Ltd.",
+        date: "Jul 2022 - Present",
+        location: "Pune, India",
+        desciption: [
+            "As a System Engineer, I provided administrative and platform support for both Linux and Windows servers, managing critical applications such as Informatica MDM, Data Exchange Services, Business Intelligence tools, Qlik, and Erwin.",
+            "My responsibilities included server configuration, performance tuning, system monitoring, troubleshooting, and ensuring seamless operation across multiple environments.",
+            "I collaborated closely with cross-functional teams to ensure the high availability, security, and scalability of systems while supporting the integration and maintenance of various applications to meet business need."
+        ]
+    }
+]
+
+export const education = [
+    {
+        title: "Bachelor's degree in Aeronautical Engineering",
+        college: "Annasaheb Dange College of Engineering and Technology, Ashta",
+        date: "2019 - 2022",
+        marks: 'GPA - 8.54 / 10'
+    },
+    {
+        title: "Diploma in Mechanical Engineering",
+        college: "Government Polytechnic, Solapur",
+        date: "2016 - 2019",
+        marks: 'Percentage - 86.6 / 100'
+    },
+]
+
+export const skills = [
+    {
+        title: "Programming and Scripting Languages",
+        skillArr: ["JavaScript", "HTML", "CSS", "Java"]
+    },
+    {
+        title: "Libraries and Frameworks",
+        skillArr: ["React.js", "Node.js","Express", "Tailwind CSS", "Bootstrap CSS"]
+    },
+    {
+        title: "Database",
+        skillArr: ["Oracle DB", "MongoDB", "PostgreSQL"]
+    },
+    {
+        title: "Database languages",
+        skillArr: ["SQL", "NoSQL", "PL-SQL"]
+    },
+    {
+        title: "Operating Systems",
+        skillArr: ["Unix", "Windows"]
+    },
+    {
+        title: "Other Tools and Technologies",
+        skillArr: ["AWS", "Git", "GitHub", "REST APT", "VS Code", "NPM", "JIRA", "Postman"]
+    },
+]
