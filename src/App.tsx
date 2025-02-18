6adc66c9368a94f517ead15ae84e342b5bd94732
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-02-18 23:10:55 +0530

    Add App component to structure main layout with navigation and sections

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+// src/App.tsx
+import React from 'react';
+import { CoolMode } from './components/ui/Particles';
+import { LandingHero } from './components/sections/Hero';
+import { ThemeToggle } from './components/ui/theme-toggle';
+import './App.css'
+import { StackingNavbar } from './components/ui/stacking-navbar';
+import { AboutSection } from './components/sections/About';
+import { MarqueeStrip } from './components/ui/marquee-strip';
+import { Work } from './components/sections/Work';
+
+const App: React.FC = () => {
+  return (
+    <div className='min-h-screen w-full bg-light-gradient bg-dark-gradient text-primary overflow-x-hidden'>
+
+      <nav className="fixed top-0 w-full p-4 flex justify-between gap-4 z-50">
+        <StackingNavbar />
+        <ThemeToggle />
+      </nav>
+
+
+      <CoolMode>
+        <div>
+          <div id="home" className="w-full h-screen flex items-center justify-center">
+            <LandingHero />
+          </div>
+
+          <div id="about" className="sm:h-[140vh] md:h-[200vh] lg:h-screen flex flex-col">
+            <h1 className='text-3xl text-muted pt-16 pl-4 md:pl-10'>About Me</h1>
+            <AboutSection />
+            <MarqueeStrip />
+          </div>
+
+          <div id="work" className="h-screen flex flex-col">
+            <h1 className='text-3xl text-muted pt-16 pl-10'>Work Experience</h1>
+            <Work />
+          </div>
+
+          <div id="projects" className="h-screen flex flex-col">
+            <h1 className='text-3xl text-muted pt-16 pl-10'>Project Showoff</h1>
+          </div>
+
+          <div id="contact" className="h-screen flex flex-col">
+            <h1 className='text-3xl text-muted pt-16 pl-10'>Contact</h1>
+          </div>
+        </div>
+      </CoolMode>
+
+    </div>
+  );
+};
+
+export default App;
