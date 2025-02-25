380667e6a9e2e3cae89db0680714332eea792298
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-02-25 12:37:29 +0530

    Update ProjectCard component styles for improved UI consistency

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -12,9 +12,9 @@ function ProjectCard({project} : {project: ProjectInterface}) {
   const toggleIsShowingMore = () => setIsShowingMore(prev => !prev);
 
   return (
-      <div className='h-fit w-full bg-white dark:bg-neutral-950 border border-transparent dark:border-neutral-800 relative z-50 flex flex-col flex-1 overflow-hidden'>
+      <div className='h-fit w-full bg-[#ffffff5d] dark:bg-[#0a0a0a5d] border border-transparent dark:border-neutral-800 relative z-50 flex flex-col flex-1 overflow-hidden'>
           <div className='flex flex-col '>
-            <h4 className="flex flex-col gap-2 items-center text-primary bg-gray-100 dark:bg-neutral-900 p-4 font-bold text-center mb-8">
+            <h4 className="flex flex-col gap-2 items-center text-primary bg-[#f3f4f65d] dark:bg-[#1717175d] p-4 font-bold text-center mb-8">
               <span className="md:text-xl w-fit px-1 py-0.5 rounded-md bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 border border-gray-200">
                 {project.title}
               </span>
@@ -63,7 +63,7 @@ function ProjectCard({project} : {project: ProjectInterface}) {
               </div>
               )}
             </div>
-            <div className={`p-4 flex flex-col gap-y-2 mx-auto ${!isShowingMore ? 'h-fit' : 'h-[50vh] md:h-fit overflow-y-auto scrollbar-hidden'} `}>
+            <div className={`p-4 flex flex-col gap-y-2 text-primary mx-auto ${!isShowingMore ? 'h-fit' : 'h-[50vh] md:h-fit overflow-y-auto scrollbar-hidden'} `}>
               <div ref={descRef} className={`break-words text-start gap-y-1 text-sm ${!isShowingMore && 'line-clamp-3'}`}>
                 { project.desciption.point.map((pnt, idx) => (
                   <p key={idx}>{`> ${pnt}`}</p>))
@@ -74,7 +74,7 @@ function ProjectCard({project} : {project: ProjectInterface}) {
               </button>
             </div>
           </div>
-          <div className={`gap-4 flex ${project.link ? "justify-between" : "justify-end" } p-4 bg-gray-100 dark:bg-neutral-900`}>
+          <div className={`gap-4 flex ${project.link ? "justify-between" : "justify-end" } p-4 bg-[#f3f4f65d] dark:bg-[#1717175d]`}>
             {project.link && <Button
               variant="secondary"
               className="w-28 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300"
